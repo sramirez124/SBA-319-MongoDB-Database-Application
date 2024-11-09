dff6b5c28c10be2f077d71da9d77ec8e19358fef
sramirez124 <forwardemails124@gmail.com>
2024-11-08 22:29:13 -0600

    more being added

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+        res.send('Its working!')
+})
+    
+app.listen(PORT, () => {
+        console.log(`Matrix is running on port: ${PORT}`)
+})
+
